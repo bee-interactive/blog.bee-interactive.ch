ae072d674f36087fac47d05dfdddba97557d0e55
Yves Engetschwiler <info@bee-interactive.ch>
2021-03-11 03:24:16 +0100

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,13 @@ import Link from 'next/link'
 
 function TailwindMark({ className }) {
   return (
-    <svg className={className} fill="none" viewBox="0 0 55 33">
-      <path fill="#fff" d="M0 0h55v33H0z" />
-      <path
-        fill="#16BDCA"
-        fillRule="evenodd"
-        d="M27.5 0c-7.333 0-11.917 3.667-13.75 11 2.75-3.667 5.958-5.042 9.625-4.125 2.092.523 3.587 2.04 5.242 3.72 2.697 2.737 5.817 5.905 12.633 5.905 7.333 0 11.917-3.667 13.75-11-2.75 3.667-5.958 5.042-9.625 4.125-2.092-.523-3.587-2.04-5.242-3.72C37.436 3.166 34.316 0 27.5 0zM13.75 16.5c-7.333 0-11.917 3.667-13.75 11 2.75-3.667 5.958-5.042 9.625-4.125 2.092.523 3.587 2.04 5.242 3.72C17.564 29.834 20.684 33 27.5 33c7.333 0 11.917-3.667 13.75-11-2.75 3.667-5.958 5.042-9.625 4.125-2.092-.523-3.587-2.04-5.242-3.72-2.697-2.738-5.817-5.905-12.633-5.905z"
-        clipRule="evenodd"
-      />
-    </svg>
+    <img className={className} src="https://bee-interactive.ch/svg/logo.svg" width="120" />
   )
 }
 
 function TailwindLogo({ className }) {
   return (
-    <img src="https://bee-interactive.ch/svg/logo.svg" width="200" />
+    <img className={className} src="https://bee-interactive.ch/svg/logo.svg" width="200" />
   )
 }
 
